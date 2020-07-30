a342bcc4ce7e432da1c3c3e5e20e368e7f1d3d95
fummicc1 <fumiya.tennis1@gmail.com>
2020-07-30 23:24:05 +0900

    Initial Commit

---
+++ EmageConvert/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  EmageConvert
+//
+//  Created by Fumiya Tanaka on 2020/07/30.
+//  Copyright © 2020 Fumiya Tanaka. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
